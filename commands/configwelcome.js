a75f5ded20bcaf38ea2e3d56cd7160e081223621
STRAXXYHUBBZ <101961334+STRAXXYHUBBZ@users.noreply.github.com>
2022-11-19 13:36:48 -0600

    Update configwelcome.js

--- commands/configwelcome.js
+++ commands/configwelcome.js
@@ -18,79 +18,81 @@ module.exports = {
 		  ),
 	async execute(interaction) {
 		
-
+		if (interaction.options.getSubcommand() === 'channel'){
+			interaction.reply("TEST")
+		}
 		
 
-if (interaction.options.getSubcommand() === 'channel') {
-		// Check for admin permissions
-		if (!interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
-			interaction.reply("You do not have permission to use this command!");
-			return;
-		}
+// if (interaction.options.getSubcommand() === 'channel') {
+// 		// Check for admin permissions
+// 		if (!interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+// 			interaction.reply("You do not have permission to use this command!");
+// 			return;
+// 		}
 
-		GuildSettings.findOne({ guild_id: interaction.guild.id }, (err, settings) => {
-			if (err) {
-				console.log(err);
-				interaction.reply("An error occurred while trying to set the welcome channel!");
-				return;
-			}
+// 		GuildSettings.findOne({ guild_id: interaction.guild.id }, (err, settings) => {
+// 			if (err) {
+// 				console.log(err);
+// 				interaction.reply("An error occurred while trying to set the welcome channel!");
+// 				return;
+// 			}
 
-			if (!settings) {
-				settings = new GuildSettings({
-					guild_id: interaction.guild.id,
-					welcome_channel_id: interaction.options.getChannel("welcome").id
-				});
-			} else {
-				settings.welcome_channel_id = interaction.options.getChannel("welcome").id;
-			}
+// 			if (!settings) {
+// 				settings = new GuildSettings({
+// 					guild_id: interaction.guild.id,
+// 					welcome_channel_id: interaction.options.getChannel("welcome").id
+// 				});
+// 			} else {
+// 				settings.welcome_channel_id = interaction.options.getChannel("welcome").id;
+// 			}
 
-			settings.save(err => {
-				if (err) {
-					console.log(err);
-					interaction.reply("An error occurred while trying to set the welcome channel!");
-					return;
-				}
+// 			settings.save(err => {
+// 				if (err) {
+// 					console.log(err);
+// 					interaction.reply("An error occurred while trying to set the welcome channel!");
+// 					return;
+// 				}
 
-				interaction.reply(`Welcome channel has been set to <#${interaction.options.getChannel("welcome").id}>`);
-			})
-		})
-	}
+// 				interaction.reply(`Welcome channel has been set to <#${interaction.options.getChannel("welcome").id}>`);
+// 			})
+// 		})
+// 	}
 
 
-	if (interaction.options.getSubcommand() === 'welcomemessage') {
-		// Check for admin permissions
-		if (!interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
-			interaction.reply("You do not have permission to use this command!");
-			return;
-		}
+// 	if (interaction.options.getSubcommand() === 'welcomemessage') {
+// 		// Check for admin permissions
+// 		if (!interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])) {
+// 			interaction.reply("You do not have permission to use this command!");
+// 			return;
+// 		}
 
-		GuildSettings.findOne({ guild_id: interaction.guild.id }, (err, settings) => {
-			if (err) {
-				console.log(err);
-				interaction.reply("An error occurred while trying to set the welcome channel!");
-				return;
-			}
+// 		GuildSettings.findOne({ guild_id: interaction.guild.id }, (err, settings) => {
+// 			if (err) {
+// 				console.log(err);
+// 				interaction.reply("An error occurred while trying to set the welcome channel!");
+// 				return;
+// 			}
 
-			if (!settings) {
-				settings = new GuildSettings({
-					guild_id: interaction.guild.id,
-					welcome_message: interaction.options.getChannel("welcomemessage").id
-				});
-			} else {
-				settings.welcome_message = interaction.options.getChannel("welcomemessage").id;
-			}
+// 			if (!settings) {
+// 				settings = new GuildSettings({
+// 					guild_id: interaction.guild.id,
+// 					welcome_message: interaction.options.getChannel("welcomemessage").id
+// 				});
+// 			} else {
+// 				settings.welcome_message = interaction.options.getChannel("welcomemessage").id;
+// 			}
 
-			settings.save(err => {
-				if (err) {
-					console.log(err);
-					interaction.reply("An error occurred while trying to set the welcome channel!");
-					return;
-				}
+// 			settings.save(err => {
+// 				if (err) {
+// 					console.log(err);
+// 					interaction.reply("An error occurred while trying to set the welcome channel!");
+// 					return;
+// 				}
 
-				interaction.reply(`Welcome message has been set to ``${interaction.options.getChannel("welcomemessage").id}```);
-			})
-		})
-	}
+// 				interaction.reply(`Welcome message has been set to ``${interaction.options.getChannel("welcomemessage").id}```);
+// 			})
+// 		})
+// 	}
 
 	}
 }
